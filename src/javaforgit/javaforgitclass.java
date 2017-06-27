8ccac2915f730735dd166fbf2f519068384427ad
wgit-project <carry111_wang@163.com>
2017-06-27 16:57:43 +0800

    java for git test ,the first time

---
+++ src/javaforgit/javaforgitclass.java
@@ -0,0 +1,11 @@
+package javaforgit;
+
+public class javaforgitclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("it is java for git test project");
+
+	}
+
+}
