24575e7eab8f38127655eece1d392c32c3cd8026
wgit-project <carry111_wang@163.com>
2017-06-27 17:14:35 +0800

    what a fuck ,blabla

--- src/javaforgit/javaforgitclass.java
+++ src/javaforgit/javaforgitclass.java
@@ -5,7 +5,7 @@ public class javaforgitclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("it is java for git test project");
-
+		System.out.println("fuck, it is not OK");
 	}
 
 }
